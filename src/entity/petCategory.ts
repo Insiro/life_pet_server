70db8c6abbc4cf0dc431cdc7650701ff7d0dd144
Insiro <leeon@insiro.me>
2022-04-06 16:11:16 +0000

    fix:wrong entity

---
+++ src/entity/petCategory.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from "typeorm";
+@Entity()
+export class PetCategory extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+  @Column()
+  name!: string;
+  @Column()
+  image1!: string; //require to specifitcation Datatype of Images
+  @Column()
+  image2!: string;
+  @Column()
+  image3!: string;
+  @Column()
+  interaction1!: string;
+  @Column()
+  interaction2!: string;
+  @Column()
+  interaction3!: string;
+}
