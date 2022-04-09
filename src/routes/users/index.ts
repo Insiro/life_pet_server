e5e9dcb28007352ca4a49191917e6d73cdb13e0b
Insiro <leeon@insiro.me>
2022-04-09 05:22:46 +0000

    add: scefolding for user router

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -3,11 +3,20 @@ import { pet_list } from "./controller";
 
 const userRouter = Router();
 
+//TODO: get User Info
+userRouter.get("/:id");
+//TODO:Delete User
+userRouter.delete("/:id");
+//TODO: update User Info
+userRouter.patch("/:id");
 
 userRouter.get("/:id/pet", pet_list);
-userRouter.use("/:id/habbit");
-userRouter.use("/:id/friend");
-userRouter.use("/:id/achivement");
-userRouter.use("/:id/habbit");
+
+//TODO: habbit for User
+userRouter.get("/:id/habbit");
+//TODO: Freintds for User
+userRouter.get("/:id/friend");
+//TODO: Achivement for User
+userRouter.get("/:id/achivement");
 
 export { userRouter };
